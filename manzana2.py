0a0d8607286c0e3e899bb6bcbf54c8830b5c055b
Miguel Torres <migmigueltorreshernandes@gmail.com>
2024-02-20 15:45:25 -0600

    manzana if,else

---
+++ manzana2.py
@@ -0,0 +1,19 @@
+cantidad = 0 
+precio = 0
+descuento = 0
+
+precio =float(input("Que precio tiene las manzanas? "))
+cantidad =int(input("Hola, cuantas manzanas deseas comprar: "))
+
+if cantidad == 18:
+    descuento =(cantidad * precio)*.15
+    print("descuento secreto")
+    print(f"vas a pagar {(cantidad * precio)-descuento}")
+    print(f"el descuentio es de: {descuento}")
+elif cantidad >= 10:
+    descuento = (cantidad * precio)*.10
+    print("Vas a pagar: ", (cantidad*precio) - descuento)
+    print(f"el descuentio es de: {descuento}")
+else:
+    print("gracias por comprar")
+    print("Vas a pagar: ", (cantidad*precio))
