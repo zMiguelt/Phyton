dd97b5e8fe370f65bddfb2153bf06bbc12c2b570
Miguel Torres <migmigueltorreshernandes@gmail.com>
2024-02-27 15:33:31 -0600

    funciones, suma y mul

---
+++ funcion2.py
@@ -0,0 +1,9 @@
+def sumar(num1,num2):
+    print(f"suma {num1+num2}")
+    print(f"multiplicacion {num1*num2}")
+    
+num1 = int(input("Ingrese el primer numero: "))
+num2 =int(input("ingrese el segundo numero: "))   
+
+sumar(num1,num2)
+print("estoy sumando en una funcion")
