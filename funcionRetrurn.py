f0ab128dfd2a110204e3ab928f18425cee8fc6e6
Miguel Torres <migmigueltorreshernandes@gmail.com>
2024-02-27 15:41:07 -0600

    Una funcion con return

---
+++ funcionRetrurn.py
@@ -0,0 +1,9 @@
+def suma(num1,num2):
+    total=num1+num2
+    print(f"el total es: {total}")
+    print("estoy imprimiendo desde una funcion ")
+    return total
+
+resultado = suma(5,5)
+print(f"este es el total fuera de la funcion: {resultado}")
+
