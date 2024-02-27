6c5eab1070cd5545742a8d7d41139bd107a8b1e0
Miguel Torres <migmigueltorreshernandes@gmail.com>
2024-02-27 15:33:11 -0600

    funciones

---
+++ funciones.py
@@ -0,0 +1,13 @@
+nombre = "raul"
+idioma = "aleman"
+def saludo(nombreFuncion,idioma):
+   # nombre="xochilt"
+    print(nombreFuncion)
+    print(f"buen dia {idioma}")
+    #print(nombre)
+    #rint("buen dia ")
+    print("===================")
+
+saludo(nombre,idioma)
+saludo(nombre,"aleman")
+saludo("felipe","frances")
